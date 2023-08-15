47a1195af9b72d1a8056539289a99a1474eec995
mutanim24 <mutanim24@gmail.com>
2023-08-15 20:08:00 +0600

    change by tanim

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section className="bg-primary text-light py-16 md:py-24">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl md:text-4xl font-semibold mb-4">Welcome to Bug Tracker Tan</h2>
+        <h2 className="text-3xl md:text-4xl font-semibold mb-4">Welcome to Bug Tracker Tanim</h2>
         <p className="text-lg md:text-xl text-tertiary mb-8">
           Your all-in-one solution for efficient bug tracking and project management.
         </p>
