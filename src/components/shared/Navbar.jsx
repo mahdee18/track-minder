75c1c1c6c2b7d664096fa319b892004c1c7a8a6e
Sulthanmahmud Suvo <sulthansuvo72@gmailcom>
2023-08-18 09:10:01 +0600

    complete authentication part

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                     />
                     <button
                       onClick={handleLogOut}
-                      className="text-gray-400 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-400 hover:bg-green-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Logout
                     </button>
